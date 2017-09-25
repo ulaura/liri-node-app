5f33ecaa88b7bdf89cd20c89402e6459d042c9b9
Laura Unaeze <l.unaeze5@gmail.com>
2017-09-24 20:10:37 -0700

    Getting responses from Twitter API, but not specifically what I want.

--- keys.js
+++ keys.js
@@ -1,10 +1,12 @@
 console.log('this is loaded');
 
-var twitterKeys = {
+var Twitter = require("twitter");
+
+var twitterKeys = new Twitter ({
   consumer_key: 'pB7wuoGgNeVytiwOTcM5rjFoo',
   consumer_secret: 'y5jGzDPc3tWRmirYvQFVgQLlHxFLsC8jJJFXb9baXzVotnNKfa',
   access_token_key: '912111857481031680-r2o82iL1gLq3lL619BbhnWIhW8SnI4g',
   access_token_secret: 'CB0DfVuIlvauU6fqX4sD9zl6Dkgs0xDhew6tdxWgz7jO8',
-}
+});
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,5 +1,23 @@
 // to access the twitter keys in keys.js
-var keys = require("./keys.js");
+var twitter = require("./keys.js");
 
 //this variable will be used to store user commands
+/*command options are as follows:
+	my-tweets
+	spotify-this-song
+	movie-this
+	do-what-it-says
+*/
 var userCommand = process.argv[2];
+
+
+//working on twitter calls first
+var myTweets = {screen_name: "LauraTest0714"};
+
+twitter.get("statuses/user_timeline", myTweets, function(error, tweets, response){
+	if (userCommand === "my-tweets" && !error) {
+		console.log(JSON.stringify(tweets, null, 2));
+	} else {
+		console.log(error);
+	}
+});
