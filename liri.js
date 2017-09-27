5d08fa7e091bcb394f13cab98ffcdfd0da24c2cc
Laura Unaeze <l.unaeze5@gmail.com>
2017-09-27 05:50:37 -0700

    Had to back up to a working point in my app. Twitter call is done.

--- liri.js
+++ liri.js
@@ -11,11 +11,12 @@ var twitter = require("./keys.js");
 var userCommand = process.argv[2];
 
 
-//working on twitter calls first. userCommand = my-tweets
+//working on twitter calls first
 var myTweets = {screen_name: "LauraTest0714"};
 
 twitter.get("statuses/user_timeline", myTweets, function(error, tweets, response){
 	if (userCommand === "my-tweets" && !error) {
+
 		for (var i = 0; i < tweets.length; i++) {
 
 			//convert UTC time to local time for tweet timestamp part 1
@@ -25,12 +26,11 @@ twitter.get("statuses/user_timeline", myTweets, function(error, tweets, response
 
 			//convert UTC time to local time for tweet timestamp part 2
 			console.log(tweetDate.toString()); 
-			console.log("- - - - - - - - - - - - - - - - - - - - -");
+			console.log("------------------------------------------");
 
 			//console.log(JSON.stringify(tweets, null, 2)); << prints out entire response of call. saving for reference.
 		}
-		
-		
+
 	} else {
 		console.log(error);
 	}
